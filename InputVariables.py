624594522c5160513646c68c7ff9656b6d94a18c
ljkootte <122276254+ljkootte@users.noreply.github.com>
2023-05-24 15:00:07 +0200

    Update InputVariables.py

--- InputVariables.py
+++ InputVariables.py
@@ -1,32 +1,35 @@
 # -*- coding: utf-8 -*-_
 """
-Created on Sun Apr  8 14:03:40 2018
-
-@author: luuki
+@author: L.J. Kootte
+@email: luckootte@gmail.com
+Developed for the PhD Thesis at TUDelft supervised by Prof.dr. C. Bisagni and Prof.dr. C. Kassapoglou
 """
 import numpy as np
 import pandas as pd
-from BasicFunctions import GetMM,CalculateTauc2
+from BasicFunctions import GetMM
 
 #Variables
 
-MPB = '3PB';  ElType   = 'SC8R' ; Shape = 'Doubler' #Hat, Doubler, Coupon, Plate
-Str_Lay = 'Thick'; Sk_Lay = 'Thin'
-Coh = True; superpose = True;  
-cpu = 32 if Coh else 4 ; mem =  None # number of cores and gb ram
+MPB = '3PB';  # 3PB, 4PB, 6PB, 7PB, SSCS, DCB, MMB, FSP, Test #ENF is 3PB+Coupon, ECT = 4PB + Coupon. 4PB actually means four-point twisting.
+Shape = 'Doubler' #Hat, Doubler, Coupon, Plate
+ElType   = 'SC8R' 
+Str_Lay = 'Thick' 
+Sk_Lay = 'Thin'
+Coh = True; superpose = True 
 Coh_mat = 'G0' ; Coh_Tri = 'GSS'
+cpu = 32 if Coh else 4 ; mem =  None # number of cores and gb ram
 
-Mesh_glo = 2.0
+Mesh_glo = 2.0 #global mesh size, not where the cohesive elements are.
 if not Coh: El_Edge = 0.3
-ttt = {'Skin':1,'Stringer':1}
+ttt = {'Skin':1,'Stringer':1} #elements through-the-thickness, number of plies in the laminate must be divisible by this number (aka 12 plies, ttt can be 1/2/3/4/6/12)
 
 if Shape=='Coupon':
-    Mesh_fla = 0.1
+    Mesh_fla = 0.1 # mesh of the flange.
     ttt['Stringer'] = ttt['Skin']
 else:  
     Mesh_fla = 0.3 if Coh else Mesh_glo
 
-Plydrop = 0.#  # drop a ply every x mm to create a taper, needs to be zero for large mesh size and element through-the-thickness.
+Plydrop = 0.#  # drop a ply every x mm to create a taper, needs to be zero for large mesh size combined with element through-the-thickness.
 
 
 if MPB == '7PB':
@@ -36,33 +39,31 @@ if MPB == '7PB':
         sk_L    = 180  # Skin length
         Sup_X   = 20   # mm from bottom side
         Sup_Y   = 20   # mm from right side
-        Load_Y  = 34
+        Load_Y  = 34   # mm from bottom side
     elif Str_Lay=='Thick': 
         sk_L    = 140  # Skin length
         Sup_X   = 24   # mm from bottom side
         Sup_Y   = 36   # mm from right side
-        Load_Y  = 36
+        Load_Y  = 36   # mm from bottom side
     Load_X  = None # mm from bottom
     ind_ra  = 12.5 ; ind_L = 4
     Surf_sq = ind_ra # mm square around the loading and support points for interaction 
-
-
 
 elif MPB == '6PB':
     sk_W    = 254  # Skin width 
     sk_L    = 260  # Skin length
     Sup_X   = 38.5 # mm from bottom side
     Sup_Y   = 60   # mm from right side
-    Load_X  = None # mm from bottom
-    Load_Y  = 60
+    Load_X  = None # mm from right side
+    Load_Y  = 60   # mm from bottom side
 
 elif MPB == 'SSCS':
     sk_W    = 254  # Skin width 
     sk_L    = 240  # Skin length
 
 elif MPB == '4PB':
-    if Shape == 'Doubler': sk_W = 127 #127
-    elif Shape == 'Hat':   sk_W = 254 #sk_W = 254   
+    if Shape == 'Doubler': sk_W = 127
+    elif Shape == 'Hat':   sk_W = 254
     else: sk_W = 220
     if Str_Lay=='Thin':    
         sk_L    = 140  # Skin length
@@ -86,16 +87,16 @@ elif MPB == '4PB':
         Sup_Y2 = sk_W-15
         
 elif MPB == '3PB':
-    sk_W    = 254#9*25.4  # Skin width
-    sk_L    = 25.4  # Skin length
-    Sup_Y   = 34#(sk_W-8*25.4)/2   # mm from bottom side
+    sk_W    = 254  # Skin width
+    sk_L    = 25.4 # Skin length
+    Sup_Y   = 34   # mm from bottom side
     R_bot   = 25.4
     R_top   = 6.35
     
     if Shape == 'Coupon': 
         sk_W      = 125  # Skin width
-        sk_L      = 25 # Skin length
-        Precrack  = 25
+        sk_L      = 25   # Skin length
+        Precrack  = 25   # 
         Sup_Y     = 25   # mm from bottom side
         R_bot   = 3
         R_top   = 3
@@ -107,12 +108,12 @@ elif MPB=='DCB':
 
 elif MPB=='MMB':
     sk_W      = 125  # Skin width
-    sk_L      = Mesh_fla*2#0.2#25 # Skin length
+    sk_L      = 25 # Skin length
     Precrack  = 25
     MMB_Ydown = sk_W - 25
     MMB_Yup   = MMB_Ydown/2
     R_circ = 3
-    MM = 0.76 #0.2, 0.44, 0.76
+    MM = 0.76 #mode-mixity, in thesis after test corrections the values used were: 0.2, 0.44, 0.76
 
 elif MPB=='FSP':
     sk_W    = 770  # Skin width
@@ -121,9 +122,11 @@ elif MPB=='FSP':
 elif MPB=='Test':
     sk_L    = 2  # Skin length
 
+   
+    
 if MPB in ['4PB', '6PB', '7PB']: # indenter length and radius
     ind_ra  = 12.5  # Radius of the indenter, Delft is 12.5 mm
-    ind_L = 4 # length of the indenter, only visual, Delft is 4 mm
+    ind_L = 4 # height of the indenter, only visual, Delft is 4 mm
     Surf_sq = ind_ra # mm square around the loading and support points for interaction 1-2x radius of indenter
 elif MPB == '3PB':  Surf_sq = R_top*2   # mm square around the loading and support points for interaction  
 elif MPB == 'MMB':  Surf_sq = R_circ*2   # mm square around the loading and support points for interaction  
@@ -131,9 +134,9 @@ elif MPB == 'MMB':  Surf_sq = R_circ*2   # mm square around the loading and supp
 
 Material = {'Skin':'IM7/977-3','Stringer':'IM7/977-3'}
 
-Sym = lambda lst,mid: lst+mid+lst[::-1]
+Sym = lambda lst,mid: lst+mid+lst[::-1] #make the layup symmetric
 if (MPB=='DCB' or MPB =='MMB' or (MPB=='3PB' and Shape=='Coupon')) and (Sk_Lay =='UD' or Str_Lay=='UD'): 
-    Layup    = {'Skin':[90]*12,'Stringer':[90]*12}
+    Layup    = {'Skin':[90]*12,'Stringer':[90]*12} #note the 90 degrees, to be consistent with the other specimen. with 90 the fibers are aligned in the direction of damage propagation.
 else:
     Layup    = {'Skin':{'Thin':Sym([-45,45,0,90,-45,45],[]),\
                             'Thick':Sym([-45,45,0,90,-45,45],[])*2}[Sk_Lay],\
@@ -146,7 +149,7 @@ if Shape =='Hat':
     str_Wtop = 30.6  # Stringer top width
     str_H    = 32.7  # Stringer heigth
     str_ang  = 70    # Stringer angle degrees
-    str_HalfWidth = (str_Wfla+str_H/np.tan(str_ang*np.pi/180.))+str_Wtop/2
+    str_HalfWidth = (str_Wfla+str_H/np.tan(str_ang*np.pi/180.))+str_Wtop/2 # half the width of the hat-stringer, dependent variable.
     radius = 5
     
 elif Shape=='Doubler':
@@ -155,7 +158,7 @@ elif Shape=='Doubler':
         str_Wfla = 25.4
     else:
         str_Wfla = 27
-        str_HalfWidth = (str_Wfla+32.7/np.tan(70*np.pi/180.))+15.3
+        str_HalfWidth = (str_Wfla+32.7/np.tan(70*np.pi/180.))+15.3 #these values were chosen to match the doubler design to the hat-stringer design, here the variable can be changed.
     if MPB=='Test': sk_W = 2*(100+str_HalfWidth)
 
 elif Shape=='Coupon':
@@ -169,7 +172,7 @@ elif Shape =='Plate':
 
 
 
-if MPB=='FSP': Str_Loc = np.array([11.8,222.8,438.8,649.8])+str_HalfWidth # Locations of the stringers on the four-stringer panel.
+if MPB=='FSP': Str_Loc = np.array([ 62.6, 273.6, 489.6, 700.6]) # Locations of the stringers on the four-stringer panel.
 elif Shape=='Coupon': Str_Loc = np.array([str_HalfWidth])
 elif Shape=='Doubler' and MPB == '4PB': Str_Loc = np.array([sk_W])
 else: Str_Loc = np.array([sk_W/2])
@@ -182,8 +185,6 @@ LamData = [[1.7E-9, 164000., 8980., .32,  .45, 5010., 5010., 3000., 0.128],\
     
 Lamina = pd.DataFrame(data = LamData, index = LamName, \
             columns = ['density','E1','E2','v12','v23','G12','G13','G23','t'])
-
-
     
 if Coh:
     ## adhesive properties
@@ -199,7 +200,7 @@ if Coh:
         sigc2 = 0.7  # Secondary mode I strength
         Gss = np.array([0.64,0.66,0.62,0.93,1.29])
         BK_MM = np.array([0,0.2,0.44,0.76,1])
-        
+
         Adhesive = CalculateTauc2(Adhesive,sigc2,Coh_mat,Coh_Tri,Gss,BK_MM) 
 
     Adhesive.loc[Coh_mat,'Ksh'] = int(Adhesive.loc[Coh_mat,'KI']*(Adhesive.loc[Coh_mat,'GIc']/Adhesive.loc[Coh_mat,'GIIc'])*(Adhesive.loc[Coh_mat,'tauc']/Adhesive.loc[Coh_mat,'sigc'])**2)
