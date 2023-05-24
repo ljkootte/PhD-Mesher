dfe8387e6a9d03e952cd8a905acce1beec22f3f0
ljkootte <122276254+ljkootte@users.noreply.github.com>
2023-05-24 16:09:11 +0200

    Update Writer.py

--- Writer.py
+++ Writer.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sun Apr  8 13:45:03 2018
-
-@author: luuki
+@author: L.J. Kootte
+@email: luckootte@gmail.com
+Developed for the PhD Thesis at TUDelft supervised by Prof.dr. C. Bisagni and Prof.dr. C. Kassapoglou
 """
 from InputVariables import *
 from CohTie import FindSurface
